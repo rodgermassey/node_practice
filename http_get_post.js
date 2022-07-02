702b64263a468539b08a90aec1404d2d5c778168
rodgermassey <007rodgermassey@gmail.com>
2022-07-02 20:15:56 +0530

    Working on http get and post method to retrieve data from the post request body

--- http_get_post.js
+++ http_get_post.js
@@ -1,15 +1,26 @@
 const http = require('http')
+const url = require('url')
 
 const server = http.createServer((request, response)=>{
     if(request.method === 'GET'){
         response.write('hello how are you?');
-        response.write(request.url)
+        let data = url.parse(request.url, true).query
+        response.write('Hello '+ data.name + '! Today\'s temprature is '+data.temp)
         
     //******* response.end() is very important if you skip this the server will keep on loading********//
         response.end(); 
     }
     else if(request.method === 'POST'){
         response.write('Recieved your request')
+        let finalData='';
+        request.on('data', (body)=>{
+            console.log(body.toString('utf-8'));
+            finalData = body.toString('utf-8')
+            
+        })
+        request.on('end',()=>{
+            console.log('This is a POST request and today\'s temprature is reported by ' +finalData.name + '\n and Today\'s temprature is '+ finalData.temprature)
+        })
         response.end();
     }
 })
