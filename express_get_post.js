369f8019c1cdcf40ac1051fb5661df582418999f
rodgermassey <007rodgermassey@gmail.com>
2022-07-05 07:26:04 +0530

    Tried get request with query and param in Express

---
+++ express_get_post.js
@@ -0,0 +1,27 @@
+const { response } = require('express');
+let express = require('express');
+let app = express();
+
+app.get('/hello/:temp?',(request, response)=>{
+    console.log('I am a get request!')
+    //************THESE LINES WILL GIVE AN ERROR IF I USE ANY 2 OF THEM THERE SHOULD BE ONLY ONE OF THEM .SENDSTATUS OR STATUS OR SEND OR JSON */
+    // response.sendStatus(500) 
+    // response.status(500).json({message: 'Error has occured'})
+    // response.send('Hello') //[Error : Cannot set headers after they are sent to the client] if this line is used after response.status() or response.sendStatus() or response.json()
+    let name = request.query.name;
+    let temp = request.params.temp || 'hi';
+        response.json({
+            temp: temp,
+            name: name,
+        });
+    // response.json({message: 'Hi i am a JSON message from the server!'}) 
+})
+// app.get('usr/',(req, res)=>{
+//     console.log('I am second get request!');
+//     let name = req.query.name;
+//     response.send(name);
+// })
+
+app.listen(9000, ()=>{
+    console.log('I am listening on port 9000!')
+})
